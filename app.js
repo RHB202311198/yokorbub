ddd7370cf7b4d8cf578c61513f745ea2f0cb1e3d
RHB202311198 <151659347+RHB202311198@users.noreply.github.com>
2023-11-22 00:08:22 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+let swiperWwd = new Swiper('.wwd__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+})
